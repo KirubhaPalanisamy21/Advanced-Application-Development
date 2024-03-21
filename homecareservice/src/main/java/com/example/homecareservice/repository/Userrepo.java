b6b07919d2f619c73ffd6aec7a919231e986a858
KirubhaPalanisamy21 <kirubhapalanisamy21@gmail.com>
2024-03-21 22:40:12 +0530

    Day-7

--- homecareservice/src/main/java/com/example/homecareservice/repository/Userrepo.java
+++ homecareservice/src/main/java/com/example/homecareservice/repository/Userrepo.java
@@ -1,9 +1,11 @@
 package com.example.homecareservice.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.homecareservice.model.User;
 
-public interface Userrepo extends JpaRepository<User,Long> {
-    
+public interface Userrepo extends JpaRepository<User,Integer> {
+    Optional<User> findByEmail(String username);
 }
